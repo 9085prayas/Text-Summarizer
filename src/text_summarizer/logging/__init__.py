444ec8c1068214d171776b86e7df0b255bcd7576
Prayas <prayas9085@gmail.com>
2025-07-10 16:24:46 +0530

    created logging

---
+++ src/text_summarizer/logging/__init__.py
@@ -0,0 +1,24 @@
+import os
+import logging
+import sys
+
+log_dir = 'logs'
+log_filepath = os.path.join(log_dir,"running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
+
+
+# Set up logging
+logging.basicConfig(
+
+    level=logging.INFO, 
+
+    format='%(asctime)s  - %(levelname)s - %(module)s - %(message)s',
+
+    handlers=[
+
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+
+    ]
+    )
+logger = logging.getLogger('mylogger')
