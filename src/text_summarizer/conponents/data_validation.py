a79c7351c9f26396fab21cab0b371dc7659dfac4
Prayas9085 <101613221+9085prayas@users.noreply.github.com>
2025-08-06 01:23:05 +0530

    added Data Validation

---
+++ src/text_summarizer/conponents/data_validation.py
@@ -0,0 +1,28 @@
+from text_summarizer.logging import logger
+import os
+from text_summarizer.entities import DataValidationConfig
+
+class DataValidation:
+    def __init__(self, config: DataValidationConfig):
+        self.config = config
+
+    def validate_all_files_exist(self) -> bool:
+        try:
+            validation_status = True
+            all_files = os.listdir(os.path.join("artifacts", "data_ingestion","samsum_dataset"))
+
+            for file in all_files:
+                if file not in self.config.ALL_REQUIRED_FILES:
+                    logger.info(f"File {file} is not a required file.")
+                    validation_status = False
+                    with open(self.config.STATUS_FILE, 'a') as status_file:
+                        status_file.write(f"Validation status: {validation_status }\n")
+                else:
+                    validation_status = True
+                    with open(self.config.STATUS_FILE, 'a') as status_file:
+                        status_file.write(f"Validation status: {validation_status }\n")
+            
+            return validation_status
+        except Exception as e:
+            raise e 
+        
